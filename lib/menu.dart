712da1db6765cc720a7d8790020cdc9dd63339c3
Lary SENE <lary.sene@canal-plus.com>
2023-01-06 17:30:49 -0400

    menu + drawer

---
+++ lib/menu.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:portail_canalplustelecom_mobile/main.dart';
+import 'package:portail_canalplustelecom_mobile/prestaplus/prestaplus.screen.dart';
+
+enum Menu {
+  prestaplus(Icons.hail_rounded, "Presta+"),
+  geoeligibitlite(Icons.map, "Géo-Eligitilitée"),
+  ;
+
+  const Menu(
+    this.icondata,
+    this.label,
+  );
+  final IconData icondata;
+  final String label;
+
+  Widget tile(BuildContext context) => ListTile(
+        leading: Icon(icondata),
+        title: Text(label),
+        onTap: () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => RootContainer(
+                selectedmenu: this,
+              ),
+            )),
+      );
+  List<Tabs> get tabs => Tabs.values.where((e) => e.menu == this).toList();
+}
+
+enum Tabs {
+  list(Menu.prestaplus, Icons.list, "Liste", PrestaplusScreen()),
+  calendar(Menu.prestaplus, Icons.calendar_month, "Calendrier", PrestaplusScreen()),
+  scan(Menu.prestaplus, Icons.qr_code_2, "Scan", PrestaplusScreen()),
+  nextrdv4(Menu.geoeligibitlite, Icons.map, "Geo", PrestaplusScreen()),
+  ;
+
+  const Tabs(
+    this.menu,
+    this.icondata,
+    this.label,
+    this.view,
+  );
+  final IconData icondata;
+  final String label;
+  final Menu menu;
+  final Widget view;
+
+  Tab get tab => Tab(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Icon(icondata),
+          ),
+          Text(label),
+        ],
+      ));
+}
