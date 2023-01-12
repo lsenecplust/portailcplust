d486a08c7e715ed54e6d75b8b90bed19ce719985
Lary SENE <lary.sene@canal-plus.com>
2023-01-12 11:58:28 -0400

    laucher icons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:portail_canalplustelecom_mobile/main.app.widget.dart';
 
-import 'package:portail_canalplustelecom_mobile/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
