2d2bddd9c7df19cd440802bde9cfedf83050639b
Lary SENE <lary.sene@canal-plus.com>
2023-06-09 19:29:20 -0400

    handle bad Certs

---
+++ lib/class/badcert.dart
@@ -0,0 +1,9 @@
+ import 'dart:io';
+
+class SelSignedCertHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
--- lib/main.local.dart
+++ lib/main.local.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:portail_canalplustelecom_mobile/class/app.config.dart';
+import 'package:portail_canalplustelecom_mobile/class/badcert.dart';
 
 import 'main.app.widget.dart';
 
@@ -10,5 +13,6 @@ void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   ApplicationConfiguration.setlocal();
+  HttpOverrides.global = SelSignedCertHttpOverrides();
   runApp(const MainApp());
 }
