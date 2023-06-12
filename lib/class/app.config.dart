52182eb9554a1e686e75e81a0a58f928352108e0
Lary SENE <lary.sene@canal-plus.com>
2023-06-12 12:44:35 -0400

    rename file
    snake case warning

--- lib/class/app.config.dart
+++ lib/class/app.config.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:equatable/equatable.dart';
-import 'package:portail_canalplustelecom_mobile/class/keycloak.dart';
+import 'package:portail_canalplustelecom_mobile/class/keycloak.config.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 class ApplicationConfiguration extends Equatable {
