88a211625616782c42e39c9be11247a0fa75ca96
Lary SENE <lary.sene@canal-plus.com>
2023-07-14 11:14:05 -0400

    init scandit all env

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:scandit_flutter_datacapture_barcode/scandit_flutter_datacapture_barcode.dart';
 
 import 'main.app.widget.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+    await ScanditFlutterDataCaptureBarcode.initialize();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MainApp());
 }
--- lib/main.production.dart
+++ lib/main.production.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:portail_canalplustelecom_mobile/class/app.config.dart';
+import 'package:scandit_flutter_datacapture_barcode/scandit_flutter_datacapture_barcode.dart';
 
 import 'main.app.widget.dart';
 
@@ -8,6 +9,7 @@ import 'main.app.widget.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+    await ScanditFlutterDataCaptureBarcode.initialize();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   ApplicationConfiguration.setproduction();
   runApp(const MainApp());
