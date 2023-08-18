c20b51704a4f236c7c425d11f48688087d2af6e4
Lary SENE <lary.sene@canal-plus.com>
2023-08-18 12:37:42 -0400

    widget Error when no internet

--- lib/keycloakRedirection/platform_impl/keycloack.mobile.dart
+++ lib/keycloakRedirection/platform_impl/keycloack.mobile.dart
@@ -3,6 +3,7 @@ import 'package:oauth2/oauth2.dart';
 import 'package:portail_canalplustelecom_mobile/auth.dart';
 import 'package:portail_canalplustelecom_mobile/class/log.dart';
 import 'package:portail_canalplustelecom_mobile/keycloakRedirection/platform_impl/keycloack.base.dart';
+import 'package:portail_canalplustelecom_mobile/widgets/somethingwentwrong.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class KeycloackImpl extends BaseLogin {
@@ -40,16 +41,30 @@ class _KeycloackWebViewState extends State<_KeycloackWebView> {
   late final WebViewController controller;
   late NavigationDelegate _navigationDelegate;
   String? logmessage = "";
-
+  WebResourceError? networkError;
+  bool isNetworkError = false;
+  bool reloading = false;
   @override
   void initState() {
     _navigationDelegate = NavigationDelegate(
-      onWebResourceError: (error) => log(error.description),
+      onWebResourceError: (error) {
+        networkError = error;
+      },
+      onPageStarted: (url) {
+        networkError = null;
+      },
+      onPageFinished: (url) async {
+        await Future.delayed(const Duration(milliseconds: 500));
+        setState(() {
+          isNetworkError = networkError != null;
+          reloading = false;
+        });
+      },
       onNavigationRequest: (NavigationRequest request) async {
         log("verify code...");
         var responseUrl = Uri.parse(request.url);
         log("verify code : ${responseUrl.toString()}");
-        debugPrint(responseUrl.toString());
+        debugPrint("NavigationRequest =>$responseUrl");
 
         if (responseUrl.queryParameters['code']?.isEmpty ?? true) {
           log(null);
@@ -78,6 +93,7 @@ class _KeycloackWebViewState extends State<_KeycloackWebView> {
   void log(String? msg) => setState(() {
         logmessage = msg;
       });
+
   void loguri(Uri responseUrl) {
     try {
       if (mounted) {
@@ -99,12 +115,48 @@ class _KeycloackWebViewState extends State<_KeycloackWebView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: [
-        WebViewWidget(controller: controller),
-        Align(alignment: Alignment.bottomCenter, child: Text(logmessage ?? ""))
-      ],
-    ));
+    return Scaffold(body: Builder(builder: (context) {
+      if (isNetworkError) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SomethingWenWrong(
+                iconsize: 70,
+                line1: "Erreur de réseaux",
+                line2: "Verifiez votre connection internet",
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: reloading
+                    ? const CircularProgressIndicator()
+                    : TextButton(
+                        onPressed: () {
+                          controller.reload();
+                          setState(() {
+                            reloading = true;
+                          });
+                        },
+                        child: const Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(Icons.refresh),
+                            Text("Réessayer"),
+                          ],
+                        )),
+              )
+            ],
+          ),
+        );
+      }
+      return Stack(
+        children: [
+          WebViewWidget(controller: controller),
+          Align(
+              alignment: Alignment.bottomCenter, child: Text(logmessage ?? ""))
+        ],
+      );
+    }));
   }
 }
